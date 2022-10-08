1bce5815311e400703d9db7772c0c4b68194caca
MrBr1ghtsid3 <tsvetoslav.shalev@gmail.com>
2022-10-08 14:49:08 +0100

    Hack alert

--- scripts/Welcome.js
+++ scripts/Welcome.js
@@ -1,5 +1,7 @@
 // Inspired by https://codepen.io/XemsDoom/pen/JjOBoG
 
+alert("Chrome only!")
+
 var currentCallback;
 myMessage = "Welcome! This is my personal take on a distraction-free, no budget writing tool. A fine line to walk, between design and functionality. Currently compatible with Google Chrome, only! Yes, this project definitely needs more time and lovin but in the interim - feel free to play around and break it.";
 
@@ -8,9 +10,6 @@ window.alert = function(msg, callback){
   $('.message').text(msg);
   $('.customAlert').css('animation', 'fadeIn 1s linear');
   $('.customAlert').css('display', 'inline');
-  setTimeout(function(){
-    $('.customAlert').css('animation', 'none');
-  }, 300);
   currentCallback = callback;
 }
 
@@ -33,4 +32,3 @@ setTimeout(function(){
     });
 }, 500);
 });
-
